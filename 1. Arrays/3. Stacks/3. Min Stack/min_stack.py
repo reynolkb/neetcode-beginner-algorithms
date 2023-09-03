55378a4f446e2b134cdb9c89d8603d687af5c889
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-09-03 08:19:18 -0700

    adding min_stack

--- 1. Arrays/3. Stacks/3. Min Stack/min_stack.py
+++ 1. Arrays/3. Stacks/3. Min Stack/min_stack.py
@@ -1,28 +1,48 @@
 class MinStack(object):
-
     def __init__(self):
-        
+        self.stack = []
+        self.minStack = []
 
     def push(self, val):
-        """
-        :type val: int
-        :rtype: None
-        """
-        
+        self.stack.append(val)
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
 
     def pop(self):
-        """
-        :rtype: None
-        """
-        
+        self.stack.pop()
+        self.minStack.pop()
 
     def top(self):
-        """
-        :rtype: int
-        """
-        
+        return self.stack[-1]
 
     def getMin(self):
-        """
-        :rtype: int
-        """
+        return self.minStack[-1]
+
+
+def main():
+    methods = ["MinStack", "push", "push", "push", "getMin", "pop", "top", "getMin"]
+    values = [[], [-2], [0], [-3], [], [], [], []]
+
+    minStack = None
+    outputs = []
+
+    for i, method in enumerate(methods):
+        if method == "MinStack":
+            minStack = MinStack()
+            outputs.append(None)
+        elif method == "push":
+            minStack.push(values[i][0])
+            outputs.append(None)
+        elif method == "pop":
+            minStack.pop()
+            outputs.append(None)
+        elif method == "top":
+            outputs.append(minStack.top())
+        elif method == "getMin":
+            outputs.append(minStack.getMin())
+
+    print("Outputs:", outputs)
+
+
+if __name__ == "__main__":
+    main()
