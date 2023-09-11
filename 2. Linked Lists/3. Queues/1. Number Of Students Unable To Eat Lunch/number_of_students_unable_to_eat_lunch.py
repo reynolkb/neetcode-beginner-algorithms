358b439b3eac98a9487bfc92e9bb0f5562f6a63f
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-09-11 11:17:51 -0700

    adding number_of_students_unable_to_eat_lunch

---
+++ 2. Linked Lists/3. Queues/1. Number Of Students Unable To Eat Lunch/number_of_students_unable_to_eat_lunch.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+
+class Solution(object):
+    def countStudents(self, students, sandwiches):
+        students = deque(students)
+        sandwiches = deque(sandwiches)
+
+        # number of students who cannot eat
+        count = 0
+
+        while len(students) > 0:
+            if students[0] == sandwiches[0]:
+                students.popleft()
+                sandwiches.popleft()
+                count = 0
+            else:
+                # Send the student to the back of the queue
+                students.rotate(-1)
+                count += 1
+
+            # If count becomes equal to the length of the students queue,
+            # it means no one can eat the top sandwich
+            if count == len(students):
+                break
+
+        return len(students)
+
+
+def main():
+    sol = Solution()
+    print(sol.countStudents([1, 1, 0, 0], [0, 1, 0, 1]))  # Output should be 0
+
+
+if __name__ == "__main__":
+    main()
