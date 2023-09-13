34cb43324c2456d2abfe01335bc8ee27478cb85d
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-09-13 08:12:46 -0700

    updating implement_stack_using_queues

--- 2. Linked Lists/3. Queues/2. Implement Stack Using Queues/implement_stack_using_queues.py
+++ 2. Linked Lists/3. Queues/2. Implement Stack Using Queues/implement_stack_using_queues.py
@@ -1,29 +1,36 @@
-class MyStack(object):
+from collections import deque
 
+
+class MyStack(object):
     def __init__(self):
-        
+        self.q = deque()
 
     def push(self, x):
-        """
-        :type x: int
-        :rtype: None
-        """
-        
+        self.q.append(x)
 
     def pop(self):
-        """
-        :rtype: int
-        """
-        
+        # Iterate through the queue until the last element.
+        # We have to do last-in-first-out and only pop from the front. Therefore, we are returning rotating the deque so the last element is the first on the top so we can remove it.
+        for i in range(len(self.q) - 1):
+            """
+            Remove the front element and push it to the back.
+            Calling push function above, essentially doing self.q.append(self.q.popleft())
+            """
+            self.push(self.q.popleft())
+        # Remove and return the last element which is now at the front.
+        return self.q.popleft()
 
     def top(self):
-        """
-        :rtype: int
-        """
-        
+        # Iterate through the queue until the last element.
+        for i in range(len(self.q) - 1):
+            # Remove the front element and push it to the back.
+            self.push(self.q.popleft())
+        # Store the last element (now at the front) in 'res'.
+        res = self.q[0]
+        # Move the last element back to its original position.
+        self.push(self.q.popleft())
+        # Return the stored last element.
+        return res
 
     def empty(self):
-        """
-        :rtype: bool
-        """
-        
+        return len(self.q) == 0
