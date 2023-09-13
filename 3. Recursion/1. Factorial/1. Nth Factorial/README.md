ea8918fc982d8d2dcde1c86abff62b135edff4aa
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-09-13 08:41:49 -0700

    adding nth_factorial

---
+++ 3. Recursion/1. Factorial/1. Nth Factorial/README.md
@@ -0,0 +1,15 @@
+# Nth Factorial
+
+## Description
+Since the course has a repeat problem of Reverse Linked List, we are doing Nth Factorial which is a common problem and also in the notes. You have to use recursion to find the Nth Factorial of a number.
+
+Example 1:\
+Input:\
+`3`\
+Output:\
+`6`\
+Explanation:\
+`1 * 2 * 3`
+
+## Run Command
+`poetry run python "3. Recursion/1. Factorial/1. Nth Factorial/nth_factorial.py"`
--- 3. Recursion/1. Factorial/1. Nth Factorial/nth_factorial.py
+++ 3. Recursion/1. Factorial/1. Nth Factorial/nth_factorial.py
@@ -1,5 +1,31 @@
-def factorial(n):
-    # base case
-    if n == 1:
-        return 1
-    return n * factorial(n - 1)
+class Solution(object):
+    def factorial(self, n):
+        # base case
+        if n == 1:
+            return 1
+        return n * self.factorial(n - 1)
+
+
+def main():
+    sol = Solution()
+    num = 5
+    res = sol.factorial(num)
+    print("The factorial of " + str(num) + " is " + str(res))
+    """
+    Call stack will be built like this:
+
+    last in, first out
+    top of call stack
+    factorial(1) <-- base case, resolved first
+
+    factorial(2)
+    factorial(3)
+    factorial(4)
+
+    bottom of call stack
+    factorial(5)
+    """
+
+
+if __name__ == "__main__":
+    main()
