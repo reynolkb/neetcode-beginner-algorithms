3fb313ccae26b7f193ab114adac283bdb28c55e6
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-09-22 06:38:58 -0700

    updated comments for kth_largest_element_in_an_array

--- 4. Sorting/3. Quick Sort/1. Kth Largest Element In An Array/kth_largest_element_in_an_array.py
+++ 4. Sorting/3. Quick Sort/1. Kth Largest Element In An Array/kth_largest_element_in_an_array.py
@@ -22,6 +22,7 @@ class Solution(object):
         return nums[k]
 
     # Function to partition the array for quickselect
+    # we swap both nums[fill] with nums[i] and nums[fill] with nums[right] because we are swapping in place
     def partition(self, nums, left, right):
         # Initialize fill pointer and pivot variable
         fill, pivot = left, nums[right]
@@ -29,11 +30,13 @@ class Solution(object):
         # Loop through array to move smaller elements to the left of pivot
         for i in range(left, right):
             if nums[i] <= pivot:
-                # since fill starts at 0 and i starts at 0, we are setting fill to i, then incrementing fill by 1 for next slot
+                # Swap elements at 'fill' and 'i' to move smaller elements to the left of the pivot which is set to fill at the end.
+                # Then increment 'fill' to prepare for the next smaller element.
                 nums[fill], nums[i] = nums[i], nums[fill]
                 fill += 1
 
-        # Move the pivot element to its fill position since everything to the left will be smaller and right is initially set at the last element
+        # Swap the pivot element (initially at 'right') with the element at the 'fill' index.
+        # This places the pivot in its correct sorted position, with all smaller elements to its left.
         nums[fill], nums[right] = nums[right], nums[fill]
 
         # Return the position of the pivot
@@ -41,8 +44,8 @@ class Solution(object):
 
 
 def main():
-    nums = [9, 7, 5, 3, 1]
-    k = 4
+    nums = [6, 5, 8, 2, 1, 3]
+    k = 5
     sol = Solution()
     result = sol.findKthLargest(nums, k)
     print(f"The {k}th largest element is {result}")
