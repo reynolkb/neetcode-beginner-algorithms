7a6671e3994903387ebdcb07263c1def9dd54ff9
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-09-23 06:58:11 -0700

    added sort_colors

---
+++ 4. Sorting/4. Bucket Sort/1. Sort Colors/sort_colors.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def sortColors(self, nums):
+        low = 0
+        high = len(nums) - 1
+        mid = 0
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+        return nums
+
+
+def main():
+    nums = [2, 0, 2, 1, 1, 0]
+    sol = Solution()
+    result = sol.sortColors(nums)
+    print(f"The sorted result is {result}")
+
+
+if __name__ == "__main__":
+    main()
