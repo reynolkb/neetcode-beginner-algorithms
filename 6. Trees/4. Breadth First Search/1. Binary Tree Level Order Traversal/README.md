c92c14bcff0ef9ed7651f7791806283725cf8fcc
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-10-02 06:44:01 -0700

    adding binary_tree_level_order_traversal

---
+++ 6. Trees/4. Breadth First Search/1. Binary Tree Level Order Traversal/README.md
@@ -0,0 +1,20 @@
+# [Binary Tree Level Order Traversal](https://leetcode.com/problems/binary-tree-level-order-traversal/)
+
+## Description
+Given the `root` of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+Example 1:\
+![Example 1](example_1.jpeg)\
+Input: root = `[3,9,20,null,null,15,7]`\
+Output: `[[3],[9,20],[15,7]]`
+
+Example 2:\
+Input: `root = [1]`\
+Output: `[[1]]`
+
+Example 3:\
+Input: `root = []`\
+Output: `[]`
+
+## Run Command
+`poetry run python "6. Trees/4. Breadth First Search/1. Binary Tree Level Order Traversal/binary_tree_level_order_traversal.py"`
---
+++ 6. Trees/4. Breadth First Search/1. Binary Tree Level Order Traversal/binary_tree_level_order_traversal.py
@@ -0,0 +1,66 @@
+from collections import deque
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def levelOrder(self, root):
+        if not root:
+            return []
+
+        result = []
+        queue = deque([root])
+
+        while queue:
+            level_size = len(queue)
+            current_level = []
+
+            for i in range(level_size):
+                node = queue.popleft()
+                current_level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(current_level)
+        return result
+
+
+def create_tree(values):
+    if not values:
+        return None
+
+    root = TreeNode(values.pop(0))
+    queue = deque([root])
+
+    while values and queue:
+        current = queue.popleft()
+        left_val = values.pop(0)
+        if left_val is not None:
+            current.left = TreeNode(left_val)
+            queue.append(current.left)
+        if values:
+            right_val = values.pop(0)
+            if right_val is not None:
+                current.right = TreeNode(right_val)
+                queue.append(current.right)
+
+    return root
+
+
+def main():
+    input_values = [3, 9, 20, None, None, 15, 7]
+    root = create_tree(input_values)
+    solution = Solution()
+    output = solution.levelOrder(root)
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
