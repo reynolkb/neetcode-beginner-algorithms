c446404656a03c55696e4a428fb4d2e4d78fd9d0
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-10-03 06:38:53 -0700

    updating binary_tree_right_side_view

--- 6. Trees/4. Breadth First Search/2. Binary Tree Right Side View/binary_tree_right_side_view.py
+++ 6. Trees/4. Breadth First Search/2. Binary Tree Right Side View/binary_tree_right_side_view.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -7,7 +10,52 @@ class TreeNode(object):
 
 class Solution(object):
     def rightSideView(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
+        if not root:
+            return []
+
+        result = []
+        queue = deque([root])
+
+        while queue:
+            level_length = len(queue)
+            for i in range(level_length):
+                node = queue.popleft()
+                if i == level_length - 1:
+                    result.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return result
+
+
+def deserialize(data):
+    if not data:
+        return None
+    root = TreeNode(data[0])
+    queue = deque([root])
+    i = 1
+    while queue and i < len(data):
+        node = queue.popleft()
+        if data[i] is not None:
+            node.left = TreeNode(data[i])
+            queue.append(node.left)
+        i += 1
+        if i < len(data) and data[i] is not None:
+            node.right = TreeNode(data[i])
+            queue.append(node.right)
+        i += 1
+    return root
+
+
+def main():
+    data = [1, 2, 3, None, 5, None, 4]
+    root = deserialize(data)
+    solution = Solution()
+    output = solution.rightSideView(root)
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
