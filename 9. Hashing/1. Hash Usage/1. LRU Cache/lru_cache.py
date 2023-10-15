49d86c13879738a79d34ef3cf78499ff4c803d20
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-10-15 07:06:42 -0700

    adding lru_cache

--- 9. Hashing/1. Hash Usage/1. LRU Cache/lru_cache.py
+++ 9. Hashing/1. Hash Usage/1. LRU Cache/lru_cache.py
@@ -1,9 +1,82 @@
-class LRUCache:
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
 
+
+class LRUCache:
     def __init__(self, capacity: int):
-        
+        self.cap = capacity
+        self.cache = {}  # map key to node
+
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    # remove node from list
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    # insert node at right
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
 
     def get(self, key: int) -> int:
-        
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
 
     def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            # remove from the list and delete the LRU from hashmap
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+
+def process_operations(operations, values):
+    output = []
+    lru_cache_instance = None
+    for operation, value in zip(operations, values):
+        if operation == "LRUCache":
+            lru_cache_instance = LRUCache(value[0])
+            output.append(None)
+        elif operation == "put":
+            lru_cache_instance.put(value[0], value[1])
+            output.append(None)
+        elif operation == "get":
+            result = lru_cache_instance.get(value[0])
+            output.append(result)
+    return output
+
+
+def main():
+    operations = [
+        "LRUCache",
+        "put",
+        "put",
+        "get",
+        "put",
+        "get",
+        "put",
+        "get",
+        "get",
+        "get",
+    ]
+    values = [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+    output = process_operations(operations, values)
+    print(output)
+
+
+# Call the main function
+if __name__ == "__main__":
+    main()
