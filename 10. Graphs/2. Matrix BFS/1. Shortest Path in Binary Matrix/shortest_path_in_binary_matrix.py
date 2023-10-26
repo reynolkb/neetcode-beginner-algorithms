a002ddd0fc1f7c0b5a90ae62939e7f780de04c68
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-10-26 06:46:46 -0700

    adding shortest_path_in_binary_matrix

---
+++ 10. Graphs/2. Matrix BFS/1. Shortest Path in Binary Matrix/shortest_path_in_binary_matrix.py
@@ -0,0 +1,43 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        # Get the length of the grid
+        N = len(grid)
+
+        # Initialize a deque with starting point (0,0) and initial length 1
+        q = deque([(0, 0, 1)])  # r, c, length
+
+        # Initialize a set to keep track of visited cells
+        visit = set((0, 0))
+
+        # Define the directions to move in the grid
+        direct = [[0, 1], [1, 0], [0, -1], [-1, 0], [1, 1], [-1, -1], [1, -1], [-1, 1]]
+
+        # Loop until the deque is empty
+        while q:
+            # Pop the front element from deque and unpack its values
+            r, c, length = q.popleft()
+
+            # Skip if the cell is out of grid or blocked
+            if min(r, c) < 0 or max(r, c) >= N or grid[r][c]:
+                continue
+
+            # Check if we reached the destination
+            if r == N - 1 and c == N - 1:
+                return length
+
+            # Loop through all possible directions
+            for dr, dc in direct:
+                # Check if the cell is visited
+                if (r + dr, c + dc) not in visit:
+                    # Append the new cell to deque with incremented length
+                    q.append((r + dr, c + dc, length + 1))
+
+                    # Add the new cell to visited set
+                    visit.add((r + dr, c + dc))
+
+        # Return -1 if there is no path
+        return -1
