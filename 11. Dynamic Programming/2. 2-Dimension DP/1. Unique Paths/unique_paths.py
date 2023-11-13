76918c564b43467798e89aca19a785451354da97
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-11-13 07:15:17 -0800

    adding unique paths

---
+++ 11. Dynamic Programming/2. 2-Dimension DP/1. Unique Paths/unique_paths.py
@@ -0,0 +1,22 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+
+        for i in range(m - 1):
+            newRow = [1] * n
+            for j in range(n - 2, -1, -1):
+                newRow[j] = newRow[j + 1] + row[j]
+            row = newRow
+        return row[0]
+
+
+def main():
+    m = 3
+    n = 7
+    solution = Solution()
+    result = solution.uniquePaths(m, n)
+    print("Output:", result)
+
+
+if __name__ == "__main__":
+    main()
