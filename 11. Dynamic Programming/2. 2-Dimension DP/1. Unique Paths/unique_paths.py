b7da2854543f2e2f8a286cc74766d53504630594
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-11-17 09:00:55 -0800

    adding comments to uniquePaths

--- 11. Dynamic Programming/2. 2-Dimension DP/1. Unique Paths/unique_paths.py
+++ 11. Dynamic Programming/2. 2-Dimension DP/1. Unique Paths/unique_paths.py
@@ -1,12 +1,20 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        # Initialize the first row with all 1s
+        # Each cell represents the number of ways to reach that cell
         row = [1] * n
 
         for i in range(m - 1):
+            # Initialize the new row with all 1s
             newRow = [1] * n
+            # Iterate over each cell in the row from right to left, excluding the first cell
             for j in range(n - 2, -1, -1):
+                # Calculate the number of paths to the current cell
+                # It's the sum of paths from the cell to its right and the cell below it
                 newRow[j] = newRow[j + 1] + row[j]
+            # Update the current row to be the new row for the next iteration
             row = newRow
+        # Return the number of paths to reach the top-left cell
         return row[0]
 
 
