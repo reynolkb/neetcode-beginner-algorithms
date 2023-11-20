f9a0d56f6dca4bd5c42cf14e3333cb3bbe7f9ab5
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-11-20 07:59:53 -0800

    formatting updates

--- 11. Dynamic Programming/2. 2-Dimension DP/2. Unique Paths 2/unique_paths_2.py
+++ 11. Dynamic Programming/2. 2-Dimension DP/2. Unique Paths 2/unique_paths_2.py
@@ -3,16 +3,16 @@ from typing import List
 
 class Solution:
     def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
-        M, N = len(grid), len(grid[0])
-        dp = [0] * N
-        dp[N - 1] = 1
+        m, n = len(grid), len(grid[0])
+        dp = [0] * n
+        dp[n - 1] = 1
 
-        # Time: O(N*M), Space: O(N)
-        for r in reversed(range(M)):
-            for c in reversed(range(N)):
+        # Time: O(n * m), Space: O(n)
+        for r in reversed(range(m)):
+            for c in reversed(range(n)):
                 if grid[r][c]:
                     dp[c] = 0
-                elif c + 1 < N:
+                elif c + 1 < n:
                     dp[c] = dp[c] + dp[c + 1]
         return dp[0]
 
