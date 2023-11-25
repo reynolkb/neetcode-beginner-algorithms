4c930ca0011a4dfe7bb110d793953375c04a0699
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-11-25 09:25:43 -0800

    updating number_of_1_bits

--- 12. Bit Manipulation/1. Bit Operations/1. Number of 1 Bits/number_of_1_bits.py
+++ 12. Bit Manipulation/1. Bit Operations/1. Number of 1 Bits/number_of_1_bits.py
@@ -1,9 +1,13 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         res = 0
+        # looks at entire number
         while n:
-            n &= n - 1
-            res += 1
+            # n % 2 returns 0 or 1 as the remainder
+            # Since the rightmost bit in binary determines whether the number is odd or even, n % 2 directly tells us the value of the rightmost bit
+            res += n % 2
+            # shifts all bits of `n` to the right by one position, discarding the rightmost bit and moving the next bit to the rightmost position
+            n = n >> 1
         return res
 
 
