36892af1b5af79a3d28244a2fd7cb8ab22b12f06
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2023-11-27 06:47:19 -0800

    adding comments to reverse_bit

--- 12. Bit Manipulation/1. Bit Operations/3. Reverse Bits/reverse_bits.py
+++ 12. Bit Manipulation/1. Bit Operations/3. Reverse Bits/reverse_bits.py
@@ -1,8 +1,12 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
+
         for i in range(32):
+            # Right shifts 'n' by 'i' bits and isolates the rightmost bit value of n. then checks it against 1 to see if both are true
             bit = (n >> i) & 1
+
+            # Left shifts the isolated bit to its reversed position and adds it to 'res'.
             res += (bit << (31 - i))
         return res
 
